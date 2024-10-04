e96696787c982e637280d34c1face7d89888505a
among1anonymous <159406690+Ashim421@users.noreply.github.com>
2024-10-04 10:25:39 +0530

    j

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'JS Mastery',
+  title: 'Sample Website',
   description: 'JS Mastery Resources',
   other: {
     'theme-color': '#0d1117',
